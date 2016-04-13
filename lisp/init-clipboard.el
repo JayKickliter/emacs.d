948b16fcec74d9a936cce04e6930eb111576d679
Jay Kickliter <jay@kickliter.com>
2016-04-13 14:03:42 -0600

    [clipboard] use xclip instead of custom solution since it integrates more cleanly

--- lisp/init-clipboard.el
+++ lisp/init-clipboard.el
@@ -1,19 +1,4 @@
-(defun pbcopy ()
-  (interactive)
-  (call-process-region (point) (mark) "pbcopy")
-  (setq deactivate-mark t))
-
-(defun pbpaste ()
-  (interactive)
-  (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
-
-(defun pbcut ()
-  (interactive)
-  (pbcopy)
-  (delete-region (region-beginning) (region-end)))
-
-(global-set-key (kbd "C-c C-c") 'pbcopy)
-(global-set-key (kbd "C-c C-v") 'pbpaste)
-(global-set-key (kbd "C-c C-x") 'pbcut)
+(require-package 'xclip)
+(xclip-mode 1)
 
 (provide 'init-clipboard)
