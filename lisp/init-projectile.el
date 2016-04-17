e19a9d184970a95da95bdc8680822c9979a4e3f5
Jay Kickliter <jay@kickliter.com>
2016-04-17 15:30:12 -0600

    Use helm for projectile completions.

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,6 +1,8 @@
 (require-package 'projectile)
 
 (projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
 (setq projectile-switch-project-action 'neotree-projectile-action)
 
 (provide 'init-projectile)
