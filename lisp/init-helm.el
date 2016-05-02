6d5fbbd771c0c32268b0bed7539495019004029c
Jay Kickliter <jay@kickliter.com>
2016-05-01 21:19:57 -0600

    Remap M-x to helm in ansi-term

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,7 +1,10 @@
 (require-package 'helm)
-(global-set-key (kbd "M-x")                          'undefined)
-(global-set-key (kbd "M-x")                          'helm-M-x)
+(global-set-key (kbd "M-x") 'undefined)
+(global-set-key (kbd "M-x") 'helm-M-x)
 (setq helm-M-x-fuzzy-match t)
+(add-hook 'term-mode-hook
+          (lambda ()
+            (define-key term-raw-map (kbd "M-x") #'helm-M-x)))
 (helm-mode 1)
 (provide 'init-helm)
 
