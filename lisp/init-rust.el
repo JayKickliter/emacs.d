636e770422aadf1e4016de540462f7a4d7489e7f
Jay Kickliter <jay@kickliter.com>
2016-05-07 21:19:45 -0600

    Rust init wasn't requiring flycheck-rust, which was causing problems on ititial startup

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,21 +1,20 @@
 (require-package 'rust-mode)
 (require-package 'company-racer)
-(require-package 'flycheck)
+(require-package 'flycheck-rust)
 
 (with-eval-after-load 'company
   (add-to-list 'company-backends 'company-racer))
 
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
 
 (defun my-rust-mode-hook ()
   (eldoc-mode)
   (flycheck-mode)
-  (local-set-key (kbd "M-t") #'racer-find-definition))
+  (local-set-key (kbd "M-t") 'racer-find-definition))
 
 (add-hook 'rust-mode-hook 'my-rust-mode-hook)
 
 (with-eval-after-load "racer"
-  '(progn
-     (define-key racer-mode-map (kbd "M-t") #'racer-find-definition)))
+  (define-key racer-mode-map (kbd "M-t") 'racer-find-definition))
 
 (provide 'init-rust)
