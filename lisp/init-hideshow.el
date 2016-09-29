8093a7b63da85c0b9f84a3811181c924bd82faff
Jay Kickliter <jay@kickliter.com>
2016-09-29 11:46:23 -0700

    Define better hideshow bindings.

--- lisp/init-hideshow.el
+++ lisp/init-hideshow.el
@@ -1,7 +1,8 @@
 (add-hook 'hs-minor-mode-hook
           (lambda ()
-            (global-set-key (kbd "C-M-0") 'hs-hide-all)
-            (global-set-key (kbd "C-M-1") 'hs-show-all)
-            (global-set-key (kbd "C-M-9") 'hs-show-block)))
+            (global-set-key (kbd "C-c h a") 'hs-hide-all)
+            (global-set-key (kbd "C-c s a") 'hs-show-all)
+            (global-set-key (kbd "C-c s b") 'hs-show-block)
+            (global-set-key (kbd "C-c h b") 'hs-hide-block)))
 
 (provide 'init-hideshow)
