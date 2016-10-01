ffb1cc2e1bbc7d8e536c6dacb64b6bb5426b6336
Jay Kickliter <jay@kickliter.com>
2016-10-01 14:59:49 -0700

    Use better hideshow bindings

--- lisp/init-hideshow.el
+++ lisp/init-hideshow.el
@@ -2,7 +2,7 @@
           (lambda ()
             (global-set-key (kbd "C-c h a") 'hs-hide-all)
             (global-set-key (kbd "C-c s a") 'hs-show-all)
-            (global-set-key (kbd "C-c s b") 'hs-show-block)
-            (global-set-key (kbd "C-c h b") 'hs-hide-block)))
+            (global-set-key (kbd "C-c s s") 'hs-show-block)
+            (global-set-key (kbd "C-c h h") 'hs-hide-block)))
 
 (provide 'init-hideshow)
