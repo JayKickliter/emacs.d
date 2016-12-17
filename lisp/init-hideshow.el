7e1c61a5c2df8e26dbd94adeac098b5f39249c10
Jay Kickliter <jay@kickliter.com>
2016-12-17 12:45:35 -0800

    Expand blocks when running goto line or isearching

--- lisp/init-hideshow.el
+++ lisp/init-hideshow.el
@@ -5,4 +5,12 @@
             (global-set-key (kbd "C-c s s") 'hs-show-block)
             (global-set-key (kbd "C-c h h") 'hs-hide-block)))
 
+;; Show code when incremental searching
+(set hs-isearch-open t)
+
+(defadvice goto-line (after expand-after-goto-line activate compile)
+  "Run hideshow-expand affected block when using `goto-line' in a collapsed buffer."
+  (save-excursion
+    (hs-show-block)))
+
 (provide 'init-hideshow)
