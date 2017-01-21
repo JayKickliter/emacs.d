7a83f73377ea3940add3edcc9517b3af2fd91cf9
Jay Kickliter <jay@kickliter.com>
2017-01-20 17:07:09 -0800

    Add projectile-ripgrep

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,5 +1,7 @@
 (require-package 'projectile)
 (require-package 'helm-projectile)
+(require-package 'ripgrep)
+(require-package 'projectile-ripgrep)
 
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
