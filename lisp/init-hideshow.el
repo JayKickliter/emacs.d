4bd78b63904fdfb29b546b627aa4bc036bd4d4bc
Jay Kickliter <jay@kickliter.com>
2017-01-23 10:17:22 -0800

    Don't hide comments when opening c/c++ files

--- lisp/init-hideshow.el
+++ lisp/init-hideshow.el
@@ -4,7 +4,8 @@
             (global-set-key (kbd "C-c s a") 'hs-show-all)
             (global-set-key (kbd "C-c s s") 'hs-show-block)
             (global-set-key (kbd "C-c h h") 'hs-hide-block)
-            (set hs-isearch-open t)))
+            (set hs-isearch-open t)
+            (setq hs-hide-comments-when-hiding-all nil)))
 
 (defadvice goto-line (after expand-after-goto-line activate compile)
   "Run hideshow-expand affected block when using `goto-line' in a collapsed buffer."
