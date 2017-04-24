a6f571a444da4d874594bb53ef146991f6479f92
Jay Kickliter <jay@kickliter.com>
2017-04-24 09:03:14 -0700

    init-misc: full-width comment box command

---
+++ lisp/init-make.el
@@ -0,0 +1,25 @@
+;;; `init-make.el' --- Customizations to `makefile-mode'
+;;; Commentary:
+
+;;; Code:
+(defun my-makefile-indent-line ()
+  (save-excursion (forward-line 0)
+                  (cond
+                   ;; keep TABs
+                   ((looking-at "\t") t)
+                   ;; indent continuation lines to 4
+                   ((and
+                     (not (bobp))
+                     (= (char-before (1- (point))) ?\\))
+                    (delete-horizontal-space)
+                    (indent-to 4))
+                   ;; delete all other leading whitespace
+                   ((looking-at "\\s-+")
+                    (replace-match "")))))
+
+(add-hook 'makefile-mode-hook
+          (lambda ()
+            (setq-local indent-line-function 'my-makefile-indent-line)))
+
+(provide 'init-make)
+;;; init-make.el ends here
