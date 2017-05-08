a2be43c19ce9737f7b5bacb513bb8a7ad8c8f6eb
Jay Kickliter <jay@kickliter.com>
2017-05-08 16:58:13 -0700

    markdown: initialize markdown command

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,5 +3,6 @@
 (after-load 'whitespace-cleanup-mode
   (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
 
+(setq markdown-command "pandoc -f markdown_github -s")
 
 (provide 'init-markdown)
