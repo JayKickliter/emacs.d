f438068760aa669d97ac440e7fc6db05bea03a2d
Jay Kickliter <jay@kickliter.com>
2017-06-23 11:09:34 -0700

    Wrap lines in markdown mode.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,8 +1,10 @@
 (require-package 'markdown-mode)
 
-(after-load 'whitespace-cleanup-mode
-  (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
+(after-load 'whitespace-cleanup-mode (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
 
-(setq markdown-command "pandoc -f markdown_github -s")
+;; Wrap lines in markdown mode
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (visual-line-mode t)))
 
 (provide 'init-markdown)
