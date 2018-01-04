87124825e97bd935ce394d8a2d24a4e85f1b90ae
Jay Kickliter <jay@kickliter.com>
2018-01-04 10:54:45 -0800

    osx: map A-<up/down> begin/end of deffun.

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -13,11 +13,12 @@
   (global-set-key (kbd "M-˙") 'ns-do-hide-others)
   (global-set-key (kbd "A-<left>") 'left-word)
   (global-set-key (kbd "A-<right>") 'right-word)
+  (global-set-key (kbd "A-<up>") 'beginning-of-defun)
+  (global-set-key (kbd "A-<down>") 'end-of-defun)
   (global-set-key (kbd "M-<left>") 'move-beginning-of-line)
   (global-set-key (kbd "M-<right>") 'move-end-of-line)
   (after-load 'nxml-mode (define-key nxml-mode-map (kbd "M-h") nil))
   (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
   )
-
 
 (provide 'init-osx-keys)
