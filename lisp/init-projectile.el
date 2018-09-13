0ca4cb62606609b5cf5413e0eebe39ea767e0aed
Jay Kickliter <jay@kickliter.com>
2018-09-13 13:16:30 -0700

    projectile: update settings to conform with newest version.

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,14 +1,11 @@
 (require-package 'projectile)
 (require-package 'helm-projectile)
-(require-package 'ripgrep)
-(require-package 'projectile-ripgrep)
 
-(projectile-global-mode)
+(projectile-mode +1)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 (setq projectile-switch-project-action 'neotree-projectile-action)
 (setq projectile-buffers-filter-function 'projectile-buffers-with-file)
-(eval-after-load 'projectile '(define-key projectile-mode-map (kbd "C-c p s r")
-                                'projectile-ripgrep))
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (provide 'init-projectile)
