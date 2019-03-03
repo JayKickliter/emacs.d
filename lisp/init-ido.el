9709aafa2f5bf262ab005219871b142845018256
Jay Kickliter <jay@kickliter.com>
2019-03-03 10:32:59 -0800

    ido: remove ido-everwhere
    It is incompatible with Helm.

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,6 +1,4 @@
-;; Use C-f during file selection to switch to regular find-file
 (ido-mode t)
-(ido-everywhere t)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length 0)
@@ -21,9 +19,8 @@
 (setq ido-default-buffer-method 'selected-window)
 
 ;; http://www.reddit.com/r/emacs/comments/21a4p9/use_recentf_and_ido_together/cgbprem
-(add-hook 'ido-setup-hook (lambda () (define-key ido-completion-map [up] 'previous-history-element)))
-
-
-
+(add-hook 'ido-setup-hook
+          (lambda ()
+            (define-key ido-completion-map [up] 'previous-history-element)))
 
 (provide 'init-ido)
