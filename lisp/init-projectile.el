f6fb15a1c4f83f9acd2878c9fb22b4b5ce35dfb3
Jay Kickliter <jay@kickliter.com>
2019-05-13 14:13:15 -0700

    projectile: map f13 to switch buffer

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -7,5 +7,6 @@
 (setq projectile-switch-project-action 'neotree-projectile-action)
 (setq projectile-buffers-filter-function 'projectile-buffers-with-file)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "<f13>") 'projectile-switch-to-buffer)
 
 (provide 'init-projectile)
