c3c40e864bb7c3b6c2834b2a0190e70fb705c164
Jay Kickliter <jay@kickliter.com>
2019-11-14 16:29:11 -0800

    projectile: improve neotree shortcut

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -14,10 +14,9 @@
   (interactive)
   (let ((project-dir (projectile-project-root))
         (file-name (buffer-file-name)))
-    (neotree-toggle)
-    (if project-dir (if (neo-global--window-exists-p)
-                        (progn (neotree-dir project-dir)
-                               (neotree-find file-name)))
+    (if project-dir (progn (neotree)
+                           (neotree-dir project-dir)
+                           (neotree-find file-name))
       (message "Could not find git project root."))))
 
 (global-set-key [f8] 'neotree-project-dir)
